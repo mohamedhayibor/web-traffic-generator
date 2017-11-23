51216f7a0b44d74821c9d20c783781987180076e
Mohamed Hayibor <mohamedhayibor@gmail.com>
2017-11-23 01:36:15 -0500

    Add block-list feature, delete unnecessary function

--- src/web_traffic_generator/core.clj
+++ src/web_traffic_generator/core.clj
@@ -1,56 +1,78 @@
 (ns web-traffic-generator.core
-  (:gen-class))
+  (:require [clj-http.client :as client]
+            [clojure.set :as s]))
 
 ;;  -- Simple web traffic generator
-;;  -- Mimicks a user browsing the internet randomly.
+;;  -- Mimicks a user browsing the internet randomly
+;;  -- 1. randomly clicks a page
+;;  -- 2. randomly "backspace" rage back to start url (needs debugging)
+;;  --> The program crashes when it hits a 4** status code
 
-(def current-link (atom {:link ""}))
+(def links (atom {:current-link ""
+                  :back-log #{}
+                  :walked #{}
+                  :block-list #{"facebook.com" "https://facebook.com"}}))
+
 
 (defn get-dom-page
   "Makes an http request -> html dom"
   [url-link]
-  (slurp url-link))
+  (:body (client/get url-link)))
 
 
-(defn get-all-links
+(defn get-page-links
   "Gets all links from the requested page"
   [dom-page]
-  (mapv second (re-seq #"(?:href\=\")(https?:\/\/[^\"]+)(?:\")" dom-page)))
+  (let [reg #"(?:href\=\")(https?:\/\/[^\"]+)(?:\")"
+        re-seq-ed (re-seq reg dom-page)
+        lazy-links (map second re-seq-ed)]
+    (into #{} lazy-links)))
 
 
-(defn pick-random-link
-  "Picks a random link out of all in the page"
-  [all-links]
-  (if (nil? all-links)
-    (do
-      (print "No links to be found. Exiting...")
-      (System/exit 0))
-    (rand-nth all-links)))
+(defn append-to-backlog!
+  [page-links]
+  (swap! links assoc :back-log page-links))
+
+
+(defn filter-links
+  "filter out walked and blocked links"
+  []
+  (let [walked-set (:walked @links)
+        blocked-list (:block-list @links)
+        back-log (:back-log @links)
+        links-diff (s/difference back-log walked-set blocked-list)]
+    (into [] links-diff)))
 
 
 (defn web-traffic-generator
-  "Kicks the web traffic generator when provided a valid link"
+  "Kicks the web traffic generator when provided with a valid link"
   [url]
-  (if (nil? url)
-    (do
-      (print "No links to be found. Exiting...")
-      (System/exit 0))
-    (-> (get-dom-page url)
-        get-all-links
-        pick-random-link)))
+  (let [dom (get-dom-page url)
+        page-links (get-page-links dom)
+        back-log (append-to-backlog! page-links)
+        f-links (filter-links)]
+    (prn ">>>>>>>> X debug")
+    (prn f-links)
+    (rand-nth f-links)))
 
 
-;; -- Other spec details
-;; TODO: implement block list > filter already searched out link
+(defn time-stamp!
+  "Logs the current link, then puts into walked"
+  [current-link]
+  (println "Current link: " (:current-link @links))
+  (swap! links assoc :walked (conj (:walked @links) current-link)))
+
+
 (defn -main
   "runs the program, ctrl + c to exit"
   [link]
   (prn "program starts")
-  (swap! current-link assoc :link link)
+  (swap! links assoc :current-link link)
   (loop []
-    (println "current link: " (:link @current-link))
+    (time-stamp! (:current-link @links))
     (Thread/sleep (* (rand-int 6) 1000))
-    (swap! current-link assoc :link (web-traffic-generator (:link @current-link)))
+    (swap! links assoc :current-link (web-traffic-generator (:current-link @links)))
     (recur)))
 
-;; (-main "https://news.ycombinator.com")
+
+(-main "https://news.ycombinator.com")
